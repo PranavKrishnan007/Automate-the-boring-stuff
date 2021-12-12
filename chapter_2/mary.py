8d66ced926f7177df5bd4a31cd7f25ed7a98e083
Pranav Krishnan <pranavk0217@gmail.com>
2021-12-12 13:19:00 +0530

    chapter 2

---
+++ chapter_2/mary.py
@@ -0,0 +1,8 @@
+name = "Mary"
+password = "something"
+if name == "Mary":
+    print("Hello, Mary")
+    if password == "something":
+        print("Access granted")
+    else:
+        print("Access denied")
